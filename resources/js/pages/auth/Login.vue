5909bd1291c1f7dc8f896170c094f7ed6ddb52f8
YoussefHarizi <youssef.harizi@gmail.com>
2021-11-16 10:58:44 +0100

    use vform for display error validation

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -5,17 +5,20 @@
             <div class="flex justify-center -mt-10">
                 <img class="border-2 w-20 h-20 rounded-full" src="/images/ys.jpg">
             </div>
+             <form @submit.prevent="login" @keydown="form.onKeydown($event)">
             <div class="px-12 py-10">
               <div class="w-full mb-2">
                 <div class="flex items-center">
                   <i class='ml-3 fill-current text-gray-400 text-xs z-10 far fa-user'></i>
-                  <input type='text' placeholder="username" class="-mx-6 px-8  w-full border rounded px-3 py-1 text-gray-700" />
+                  <input type='email' name="email" placeholder="email" v-model="form.email" class="-mx-6 px-8  w-full border rounded px-3 py-1 text-gray-700" />
+                  <HasError :form="form" field="email" />
             	</div>
             </div>
             <div class="w-full mb-2">
                 <div class="flex items-center">
                   <i class='ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock'></i>
-                  <input type='text' placeholder="password" class="-mx-6 px-8 w-full border rounded px-3 py-1 text-gray-700" />
+                  <input type='password' name="password" placeholder="password" v-model="form.password" class="-mx-6 px-8 w-full border rounded px-3 py-1 text-gray-700" />
+                  <HasError :form="form" field="password" />
                 </div>
             </div>
             <div class="mt-8 flex justify-between">
@@ -24,16 +27,38 @@
                   <span class="text-xs text-gray-700">Remember Me</span>
                 </div>
 				<div>
-                  <button type='text' class="bg-yellow-400 text-xs text-gray-700 rounded px-4 py-2">SIGN IN</button>
+                  <button type='submit' :disabled="form.busy" class="bg-yellow-400 text-xs text-gray-700 rounded px-4 py-2">SIGN IN</button>
                 </div>
             </div>
           </div>
+             </form>
         </div>
   </div>
 </template>
 
 <script>
+import {Form} from 'vform'
+import { Button, HasError, AlertError } from 'vform/src/components/tailwind'
 export default {
+  components: {
+    Button, HasError, AlertError
+  },
+  data(){
+    return{
+      form:new Form({
+        email:'',
+        password:'',
+        remember:false
+      })
+    }
+  },
+  methods: {
+    async login () {
+      const response = await this.form.post('/api/v1/admin/login')
+      // ...
+    }
+  }
+
 
 }
 </script>
